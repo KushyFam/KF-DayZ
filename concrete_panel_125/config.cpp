44875d1f0a8cf5976575ff7463d52b029d7cfeaa
KushyFam <kushyfam@gmail.com>
2024-10-22 16:01:54 +0100

    Update config.cpp

--- concrete_panel_125/config.cpp
+++ concrete_panel_125/config.cpp
@@ -1,1 +1,19 @@
-
+class CfgPatches
+{
+	class YOUR_CLASSNAME
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=0.1;
+		requiredAddons[] = {"DZ_Data","DZ_Scripts"};
+	};
+};
+class CfgVehicles
+{
+	class HouseNoDestruct;
+	class StaticObj_concrete_panel_125: HouseNoDestruct
+	{
+		scope=0;
+		model="YOURMOD\DIRECTORY\TO\concrete_panel_125.p3d";
+	};
+};
